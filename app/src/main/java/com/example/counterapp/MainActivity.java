7ac2ba654e17cd0e37e3bd8d279f61345122421a
Hassan001 <bitf19a001@pucit.edu.pk>
2022-11-01 23:48:47 +0500

    commit01 for implementsViewDotOnClickListner

--- app/src/main/java/com/example/counterapp/MainActivity.java
+++ app/src/main/java/com/example/counterapp/MainActivity.java
@@ -2,12 +2,65 @@ package com.example.counterapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    TextView digi;
+    Button btnI, btnD, btnR;
+    int value=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        digi = findViewById(R.id.textViewer);
+        btnI = findViewById(R.id.buttonInc);
+        btnD = findViewById(R.id.buttonDec);
+        btnR = findViewById(R.id.buttonreset);
+        btnI.setOnClickListener(this);
+        btnD.setOnClickListener(this);
+        btnR.setOnClickListener(this);
+        digi.setOnClickListener(this);
     }
+
+
+    @Override
+    public void onClick(View view)
+    {
+        switch (view.getId())
+        {
+            //handle multiple view click events
+            case R.id.textViewer:
+                String s = Integer.toString(value);
+                digi.setText(s);
+                break;
+            case R.id.buttonInc:
+                value++;
+                 s = Integer.toString(value);
+                digi.setText(s);
+                break;
+            case R.id.buttonDec:
+                value--;
+                s = Integer.toString(value);
+                digi.setText(s);
+                break;
+            case R.id.buttonreset:
+                value=0;
+                s = Integer.toString(value);
+                digi.setText(s);
+                break;
+            default:
+                s = Integer.toString(value);
+                digi.setText(s);
+                break;
+        }
+
+
+
+
+    }
+
 }
